c925741e2bd744ab69e31742ea8169849c4ba4c3
Tripp <igarzotto@radersolutions.com>
2023-01-31 09:10:45 -0600

    setup editdialplan

--- src/views/ratel/administration/SetupEditDialplan.js
+++ src/views/ratel/administration/SetupEditDialplan.js
@@ -14,76 +14,70 @@ import { CippCodeBlock } from 'src/components/utilities'
 
 
 const EditDialplan = () => {
-//   const dispatch = useDispatch()
-//   let query = useQuery()
-//   const dialplan = query.get('dialplan')
-//   const name = query.get('name')
-//   const description = query.get('description')
-//   const tenantDomain = query.get('tenantDomain')
-//   const [queryError, setQueryError] = useState(false)
+  const dispatch = useDispatch()
+  let query = useQuery()
+  const dialplan = query.get('dialplan')
+  const name = query.get('name')
+  const description = query.get('description')
+  const tenantDomain = query.get('tenantDomain')
+  const [queryError, setQueryError] = useState(false)
 
-//   const [genericPostRequest, postResults] = useLazyGenericPostRequestQuery()
-//   const onSubmit = (values) => {
-//     //@todo: need to fix copyfrom in api so this is no longer required
+  const [genericPostRequest, postResults] = useLazyGenericPostRequestQuery()
+  const onSubmit = (values) => {
+    //@todo: need to fix copyfrom in api so this is no longer required
 
-//     //@todo: need to fix this in api so this hacky shit is no longer needed.
+    //@todo: need to fix this in api so this hacky shit is no longer needed.
 
-//     const shippedValues = {
-//       Name: values.Name,
-//       Description: values.Description,
-//       Dialplan: values.Dialplan,
-//     }
-//     //window.alert(JSON.stringify(shippedValues))
-//     // console.log(values)
-//     // genericPostRequest({ path: '/api/LtRatelDIDs', values: shippedValues })
-//   }
+    const shippedValues = {
+      Name: values.Name,
+      Description: values.Description,
+      Dialplan: values.Dialplan,
+    }
+    //window.alert(JSON.stringify(shippedValues))
+    // console.log(values)
+    // genericPostRequest({ path: '/api/LtRatelDIDs', values: shippedValues })
+  }
 
 
 
-//   return (
-//     <CippPage>
-//       <CCol lg={6} xs={12}>
-//         <CippContentCard title="Edit Dialplan" icon={faEdit}>
-//           <Form
-//             // initialValues={{ ...initialState }}
-//             onSubmit={onSubmit}
-//             render={({ handleSubmit, submitting, values }) => {
-//               return (
-//                 <CForm onSubmit={handleSubmit}>
-//                 <CRow><CCol>
-//                         <RFFCFormInput type="text" name="Name" label="Name"  />
-//                     </CCol>
-//                     <CCol>
-//                         <RFFCFormInput type="text" name="Description" label="Description"  />
-//                     </CCol>
-//                     <CCol>
-//                         <RFFCFormInput type="text" name="Dialplan" label="Dialplan" />
-//                     </CCol>
-//                 </CRow>
-//                   <CRow className="mb-3">
-//                     <CCol md={6}>
-//                       <CButton type="submit" >
-//                         Edit Member
+  return (
+    <CippPage>
+      <CCol lg={6} xs={12}>
+        <CippContentCard title="Edit Dialplan" icon={faEdit}>
+          <Form
+            // initialValues={{ ...initialState }}
+            onSubmit={onSubmit}
+            render={({ handleSubmit, submitting, values }) => {
+              return (
+                <CForm onSubmit={handleSubmit}>
+                <CRow><CCol>
+                        <RFFCFormInput type="text" name="Name" label="Name"  />
+                    </CCol>
+                    <CCol>
+                        <RFFCFormInput type="text" name="Description" label="Description"  />
+                    </CCol>
+                    <CCol>
+                        <RFFCFormTextarea type="text" name="Dialplan" label="Dialplan" />
+                    </CCol>
+                </CRow>
+                  <CRow className="mb-3">
+                    <CCol md={6}>
+                      <CButton type="submit" >
+                        Edit Member
                  
-//                       </CButton>
-//                     </CCol>
-//                   </CRow>
+                      </CButton>
+                    </CCol>
+                  </CRow>
            
-//                 </CForm>
-//               )
-//             }}
-//           />
-//         </CippContentCard>
-//       </CCol>
-//     </CippPage>
-//   )
-return (
-    <>
-    <div>
-        test
-    </div>
-    </>
-)
+                </CForm>
+              )
+            }}
+          />
+        </CippContentCard>
+      </CCol>
+    </CippPage>
+  )
+
 }
 
 export default EditDialplan
