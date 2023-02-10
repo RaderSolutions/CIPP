06e07a92180502d4c0d7695f4638f3d1ac6a8498
Tripp <igarzotto@radersolutions.com>
2023-02-10 08:57:00 -0600

    RatelAddDevice

--- src/views/ratel/administration/RatelAddDevice.js
+++ src/views/ratel/administration/RatelAddDevice.js
@@ -16,6 +16,7 @@ import {
 } from 'src/store/api/ratelDevices'
 import { useListDidsQuery } from 'src/store/api/ratelDids'
 import { useSelector } from 'react-redux'
+import Select from "react-select"
 
 const Error = ({ name }) => (
   <Field
