e8dec9524e7090479ea67a74fd5994b0dc57c131
Tripp <igarzotto@radersolutions.com>
2023-02-16 09:37:12 -0600

    RatelAddDevice

--- src/views/ratel/administration/RatelAddDevice.js
+++ src/views/ratel/administration/RatelAddDevice.js
@@ -359,7 +359,7 @@ const AddRatelDevice = ({ children }) => {
               {(props) => {
                 const { values } = props
                 for (let value in values) {
-                  if (value[index] !== '0') {
+                  if (value !== 'ToggleNewDidInput') {
                     console.log(value + ': ' + values[value])
                     value.split('').forEach((char, idx) => {
                       if (idx !== 0 && char === char.toUpperCase()) {
