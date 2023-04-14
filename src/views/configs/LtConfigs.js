ed4124b0bcc213db46e9cd3b4eec128aee3a36d7
Alex <63439547+smallfri321@users.noreply.github.com>
2023-04-14 14:45:33 -0500

    Dropdown

--- src/views/configs/LtConfigs.js
+++ src/views/configs/LtConfigs.js
@@ -10,80 +10,39 @@ import {
   CRow,
   CForm,
 } from '@coreui/react'
-// import {
-//   useLazyExecClearCacheQuery,
-//   useLazyExecNotificationConfigQuery,
-//   useLazyExecPermissionsAccessCheckQuery,
-//   useLazyExecTenantsAccessCheckQuery,
-//   useLazyGenericGetRequestQuery,
-//   useLazyGenericPostRequestQuery,
-//   useLazyListNotificationConfigQuery,
-// } from 'src/store/api/app'
-// import {
-//   useExecAddExcludeTenantMutation,
-//   useExecRemoveExcludeTenantMutation,
-// } from 'src/store/api/tenants'
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-  //   faCheckCircle,
-  //   faCircleNotch,
-  //   faExclamationTriangle,
-  //   faEye,
-  //   faEyeSlash,
-  //   faTrash,
-  //   faRecycle,
-  faLink,
-  faCog,
-} from '@fortawesome/free-solid-svg-icons'
-// import { useListTenantsQuery } from 'src/store/api/tenants'
-// import { useLazyEditDnsConfigQuery, useLazyGetDnsConfigQuery } from 'src/store/api/domains'
-// import { useDispatch, useSelector } from 'react-redux'
-// import { cellBooleanFormatter, CellTip, CellTipIcon, CippTable } from 'src/components/tables'
-// import { CippPage, CippPageList } from 'src/components/layout'
-import {
-  //  RFFCFormSwitch,
-  // RFFCFormInput,
-  RFFCFormSelect,
-} from 'src/components/forms'
+import { faLink, faCog } from '@fortawesome/free-solid-svg-icons'
+
+import { RFFCFormSelect } from 'src/components/forms'
 import { Form } from 'react-final-form'
-// import useConfirmModal from 'src/hooks/useConfirmModal'
-// import { setCurrentTenant } from 'src/store/features/app'
-import {
-  CippCodeBlock,
-  // ModalService,
-  // TenantSelectorMultiple
-} from 'src/components/utilities'
-// import CippListOffcanvas from 'src/components/utilities/CippListOffcanvas'
-// import { TitleButton } from 'src/components/buttons'
-// import Skeleton from 'react-loading-skeleton'
-// import { Buffer } from 'buffer'
+
+import { CippCodeBlock } from 'src/components/utilities'
+
+const ConfigList = () => {
+  const tenant = useSelector((state) => state.app.currentTenant)
+
+  // Get the list of configs from an API
+  const [configList, setConfigList] = useState([])
+
+  useEffect(() => {
+    fetch('/api/GrabConfigs')
+      .then((response) => response.json())
+      .then((data) => setConfigList(data))
+  }, [])
+
+  return configList.map((config, index) => ({
+    value: config.Number,
+    label: config.Name,
+  }))
+}
 
 const Configs = () => {
   const [selectedConfig, setSelectedConfig] = useState()
-  //   const [listConfigs] = useLazyGenericGetRequestQuery()
 
   const handleSubmit = async (values) => {
     console.log(values)
   }
-
-  // const handleGetLink = () => {
-  //   console.log('Making link')
-  //   listScriptLink({
-  //     path: 'api/ExecMaintenanceScripts',
-  //     params: { ScriptFile: selectedScript, MakeLink: 'True' },
-  //   })
-  // }
-  // const dumb = [
-  //   {
-  //     config: 'config',
-  //   },
-  //   {
-  //     config: 'config',
-  //   },
-  //   {
-  //     config: 'config',
-  //   },
-  // ]
 
   return (
     <>
@@ -107,11 +66,7 @@ const Configs = () => {
                               name="ConfigFile"
                               label="Config File"
                               placeholder="-- Select a config --"
-                              // values={dumb}
-                              values={[
-                                { value: 'name', label: 'value' },
-                                { value: 'name2', label: 'value2' },
-                              ]}
+                              values={ConfigList()}
                             />
                           </CCol>
                         </CRow>
@@ -134,52 +89,9 @@ const Configs = () => {
       </CRow>
       <CRow>
         <CCol>
-          {/* <CCard>
-              <CCardBody>
-                <Skeleton count={10} />
-              </CCardBody>
-            </CCard>
-         */}
-          {/* <CCard>
-            <CCardHeader>
-              <CCardTitle>Script Details</CCardTitle>
-            </CCardHeader>
-            <CCardBody>
-              {/* <p>
-                <CButton type="submit" onClick={handleGetLink}>
-                  <FontAwesomeIcon icon={faLink} className="me-2" />
-                  Create Link
-                </CButton>
-              </p> */}
-          {/* 
-              <p>
-                <>
-                  <p>
-                    Copy this text into a PowerShell terminal, we recommend Azure Cloud Shell. Azure
-                    modules and the az command line utilties are required for these scripts to work.
-                    The link is valid for 5 minutes.
-                  </p>
-                  <CippCodeBlock
-                    language="text"
-                    showLineNumbers={false}
-                    wrapLongLines={true}
-                    //   code={
-                    //   }
-                  />
-                </>
-              </p>
-
-              <p>
-                <h5>Config File Contents</h5>
-                <CippCodeBlock
-                  language="powershell"
-                  showLineNumbers={true}
-                  wrapLongLines={false}
-                  // code={}
-                />
-              </p>
-            </CCardBody>
-          </CCard>  */}
+          {}
+          {}
+          {}
         </CCol>
       </CRow>
     </>
