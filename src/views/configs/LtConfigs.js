5bf5d0c37390aa7d4ebab20010748ad46f385738
Alex <63439547+smallfri321@users.noreply.github.com>
2023-04-24 15:39:24 -0500

    Adjusted for backend changes

--- src/views/configs/LtConfigs.js
+++ src/views/configs/LtConfigs.js
@@ -39,8 +39,8 @@ const Configs = () => {
         console.log('response b4 JSON: ', response);
         const data = await response.json();
         console.log('Config list fetched:', data);
-        data.forEach(dat => console.log('Heres a piece! ', dat.text));
-        setConfigList(data);
+        data.forEach(dat => console.log('Heres a piece! ', dat));
+        //setConfigList(data);
       } catch (error) {
         console.error('Error fetching config list:', error);
       }
