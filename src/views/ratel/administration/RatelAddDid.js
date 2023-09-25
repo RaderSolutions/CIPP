e939422030c56617c9c6fba2abca1a738371cf8d
Ivan Garzotto <70814013+rvbv89@users.noreply.github.com>
2023-09-25 12:05:38 -0500

    RatelAddDid

--- src/views/ratel/administration/RatelAddDid.js
+++ src/views/ratel/administration/RatelAddDid.js
@@ -6,7 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
 import { CippWizard } from 'src/components/layout'
 import PropTypes, { array } from 'prop-types'
-import { RFFCFormInput, RFFCFormSelect, RFFCFormSwitch, Condition } from 'src/components/forms'
+import { RFFCFormInput, RFFCFormSelect, RFFCFormSwitch, Condition, RFFCFormTextarea } from 'src/components/forms'
 import { TenantSelector } from 'src/components/utilities'
 import { useLazyGenericPostRequestQuery } from 'src/store/api/app'
 import { useListDidsQuery } from 'src/store/api/ratelDids'
@@ -184,7 +184,7 @@ return (
                 placeholder="Enter the DID"
                 type="text"
                 />
-                <RFFCFormInput
+                <RFFCFormTextarea
                 name="Dialplan"
                 label="Dialplan"
                 placeholder="Enter the Dialplan"
