cd1385061d2a9d84c877d0ee557877ed8fad0b05
Ivan Garzotto <70814013+rvbv89@users.noreply.github.com>
2023-09-25 14:33:52 -0500

    RatelAddDid

--- src/views/ratel/administration/RatelAddDid.js
+++ src/views/ratel/administration/RatelAddDid.js
@@ -70,7 +70,7 @@ const AddRatelDid = ({ children }) => {
         //   Dialplan: values.Dialplan,
         //   DidType: values.DidType,
         // }
-        genericPostRequest({ path: `/api/LtScheduleScript?TenantFilter=${tenantDomain}&Parameters=Key=DID|Value=${values.Did},Key=notes|Value=${values.DialplanName},Key=Dialplan|Value=${values.Dialplan}&RatelScript=true&ScriptId=7532` })
+        genericPostRequest({ path: `/api/LtScheduleScript?TenantFilter=${tenantDomain}&Parameters=Key=DID|Value=${values.Did},Key=Notes|Value=${values.DialplanName},Key=Dialplan|Value=${values.Dialplan}&RatelScript=true&ScriptId=7532` })
     // genericPostRequest({
     //   path: `/api/LtScheduleScript?TenantFilter=${tenantDomain}&Parameters=Key=DID|Value=${values.Did},Key=Dialplan|Value=${values.Dialplan}&RatelScript=true&ScriptId=7352`,
     // })
