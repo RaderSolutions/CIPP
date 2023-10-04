cc55fee191b52f60e09d90bad4b9f05081ad881e
Ivan Garzotto <70814013+rvbv89@users.noreply.github.com>
2023-10-04 12:03:13 -0500

    PickupGroups

--- src/views/ratel/administration/PickupGroups.js
+++ src/views/ratel/administration/PickupGroups.js
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
-import { CButton, CCol, CCallout } from '@coreui/react'
+import { CButton, CCol, CCallout, CSpinner } from '@coreui/react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEllipsisV, faEdit } from '@fortawesome/free-solid-svg-icons'
 import { CippPageList } from 'src/components/layout'
