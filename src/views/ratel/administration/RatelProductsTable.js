818200025f2c5836f26fe5558b044c6909b42e08
Ivan Garzotto <70814013+rvbv89@users.noreply.github.com>
2023-10-13 08:55:06 -0500

    RatelProductsTable

--- src/views/ratel/administration/RatelProductsTable.js
+++ src/views/ratel/administration/RatelProductsTable.js
@@ -126,7 +126,7 @@ export const ProductsTable = () => {
             selector: (row) => row['supports_lldp'],
             sortable: true,
             exportSelector: 'SupportsLLDP',
-            Cell: (row) => row.value ? 'true' : 'false',
+            Cell: (row) => row.value === true ? 'true' : 'false',
         }
     ]
     const {
