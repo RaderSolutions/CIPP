4e79383b6d940225c185e0596dd6194c1ac334c3
Ivan Garzotto <70814013+rvbv89@users.noreply.github.com>
2023-11-06 07:05:03 -0600

    AddPagingGroup

--- src/views/ratel/administration/AddPagingGroup.js
+++ src/views/ratel/administration/AddPagingGroup.js
@@ -1,26 +1,37 @@
 /* eslint-disable prettier/prettier */
-import React, {useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
 import { CCallout, CCol, CRow, CSpinner, CListGroup, CListGroupItem } from '@coreui/react'
 import { Field, FormSpy, useFormState } from 'react-final-form'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
 import { CippWizard } from 'src/components/layout'
 import PropTypes from 'prop-types'
-import { RFFCFormInput, RFFCFormRadio, RFFCFormSelect, RFFCFormSwitch, RFFCFormTextarea } from 'src/components/forms/RFFComponents'
+import {
+  RFFCFormInput,
+  RFFCFormRadio,
+  RFFCFormSelect,
+  RFFCFormSwitch,
+  RFFCFormTextarea,
+} from 'src/components/forms/RFFComponents'
 import { TenantSelector } from 'src/components/utilities'
 import { useLazyGenericPostRequestQuery } from 'src/store/api/app'
 import { useSelector } from 'react-redux'
 
-
 const AddPagingGroup = () => {
   const [genericPostRequest, postResults] = useLazyGenericPostRequestQuery()
+  const [hideFromPB, setHideFromPB] = useState(0)
 
   const tenantDomain = useSelector((state) => state.app.currentTenant.customerId)
   const initialFormValues = {
-    DialExtension: '',         // Set the default value for DialExtension
-    PageGroupName: '',        // Set the default value for PageGroupName
-    HideFromPB: 0,         // Set the default value for HideFromPB
-  };
+    DialExtension: '', // Set the default value for DialExtension
+    PageGroupName: '', // Set the default value for PageGroupName
+    HideFromPB: hideFromPB, // Set the default value for HideFromPB
+  }
+
+  const onToggleHide = (e) => {
+    console.log('e', e)
+    setHideFromPB(!hideFromPB)
+  }
 
   const handleSubmit = async (values) => {
     alert(JSON.stringify(values, null, 2))
@@ -29,14 +40,17 @@ const AddPagingGroup = () => {
     })
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     console.log('postResults', postResults)
     console.log('prdata', postResults?.data?.Data?.Results)
-    
   }, [postResults])
 
   return (
-    <CippWizard onSubmit={handleSubmit} initialValues={{initialFormValues}} wizardTitle="Add Ratel Paging Group Wizard">
+    <CippWizard
+      onSubmit={handleSubmit}
+      initialValues={{ initialFormValues }}
+      wizardTitle="Add Ratel Paging Group Wizard"
+    >
       <CippWizard.Page
         title="Tenant Choice"
         description="Choose the tenant to add a paging group to"
@@ -68,7 +82,11 @@ const AddPagingGroup = () => {
               <RFFCFormInput name="PageGroupName" label="Page Group Name" />
             </CCol>
             <CCol lg={6} xs={12}>
-              <RFFCFormSwitch name="HideFromPB" label="Hide From Phonebook" value={1} />
+              <RFFCFormSwitch
+                name="HideFromPB"
+                label="Hide From Phonebook"
+                onChange={onToggleHide}
+              />
             </CCol>
           </CRow>
         </div>
@@ -84,57 +102,55 @@ const AddPagingGroup = () => {
               <CSpinner>Loading</CSpinner>
             </CCallout>
           )} */}
-          {
-            !postResults.isFetching && !postResults.isSuccess && (
-              <FormSpy subscription={{ values: true, labels: true }}>
-                {
-                  (props) => {
-                    const formState = useFormState()
-                    const {values} = props;
-                    const valuesArray = Object.keys(formState.values)?.map((key) => ({
-                      key: key,
-                      value: values[key]
-                    }));
+          {!postResults.isFetching && !postResults.isSuccess && (
+            <FormSpy subscription={{ values: true, labels: true }}>
+              {(props) => {
+                const formState = useFormState()
+                const { values } = props
+                const valuesArray = Object.keys(formState.values)?.map((key) => ({
+                  key: key,
+                  value: values[key],
+                }))
 
-                    console.log('valuesArray', valuesArray) 
-                    let dialExtensionKey;
-                    dialExtensionKey = valuesArray.find((item) => item.key === 'DialExtension');
-                    let pageGroupNameKey;
-                    pageGroupNameKey = valuesArray.find((item) => item.key === 'PageGroupName');
-                    let hideFromPBKey;
-                    hideFromPBKey = valuesArray.find((item) => item.key === 'HideFromPB');
-                    console.log('dialExtensionKey', dialExtensionKey)
-                    return <CListGroup>
-                      {
-                        dialExtensionKey && <CListGroupItem>
-                          {`Dial Extension: ${dialExtensionKey.value}`}
-                        </CListGroupItem>
-                      }
-                      {
-                        pageGroupNameKey && <CListGroupItem>
-                          {`Page Group Name: ${pageGroupNameKey.value}`}
-                        </CListGroupItem>
-                      }
-                      {
-                        hideFromPBKey && <CListGroupItem>
-                          {`Hide From Phonebook: ${hideFromPBKey.value}`}
-                        </CListGroupItem>
-                      }
-                    </CListGroup>
-                  }
-                }
-                </FormSpy>
-            )
-          }
-        {postResults.isFetching && (
-          <CCallout color="info">
-            <CSpinner>Loading</CSpinner>
-          </CCallout>
-        )}
-        {postResults.isSuccess || postResults.status === 'fulfilled' && <CCallout color="success">Script Run Successfully</CCallout>}
-        <hr className="my-4" />
+                console.log('valuesArray', valuesArray)
+                let dialExtensionKey
+                dialExtensionKey = valuesArray.find((item) => item.key === 'DialExtension')
+                let pageGroupNameKey
+                pageGroupNameKey = valuesArray.find((item) => item.key === 'PageGroupName')
+                let hideFromPBKey
+                hideFromPBKey = valuesArray.find((item) => item.key === 'HideFromPB')
+                console.log('dialExtensionKey', dialExtensionKey)
+                return (
+                  <CListGroup>
+                    {dialExtensionKey && (
+                      <CListGroupItem>{`Dial Extension: ${dialExtensionKey.value}`}</CListGroupItem>
+                    )}
+                    {pageGroupNameKey && (
+                      <CListGroupItem>
+                        {`Page Group Name: ${pageGroupNameKey.value}`}
+                      </CListGroupItem>
+                    )}
+                    {hideFromPBKey && (
+                      <CListGroupItem>
+                        {`Hide From Phonebook: ${hideFromPBKey.value}`}
+                      </CListGroupItem>
+                    )}
+                  </CListGroup>
+                )
+              }}
+            </FormSpy>
+          )}
+          {postResults.isFetching && (
+            <CCallout color="info">
+              <CSpinner>Loading</CSpinner>
+            </CCallout>
+          )}
+          {postResults.isSuccess ||
+            (postResults.status === 'fulfilled' && (
+              <CCallout color="success">Script Run Successfully</CCallout>
+            ))}
+          <hr className="my-4" />
         </center>
-       
       </CippWizard.Page>
     </CippWizard>
   )
