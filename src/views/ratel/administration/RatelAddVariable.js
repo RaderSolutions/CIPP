b718ca5c54857c35e1195744dc6b60bbbd3fcc1d
Ivan Garzotto <70814013+rvbv89@users.noreply.github.com>
2023-11-13 11:21:54 -0600

    RatelAddVariable

--- src/views/ratel/administration/RatelAddVariable.js
+++ src/views/ratel/administration/RatelAddVariable.js
@@ -6,7 +6,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
 import { CippWizard } from 'src/components/layout'
 import PropTypes, { array } from 'prop-types'
-import { RFFCFormInput, RFFCFormSelect, RFFCFormSwitch, Condition, RFFCFormTextarea } from 'src/components/forms'
+import {
+  RFFCFormInput,
+  RFFCFormSelect,
+  RFFCFormSwitch,
+  Condition,
+  RFFCFormTextarea,
+} from 'src/components/forms'
 import { TenantSelector } from 'src/components/utilities'
 import { useLazyGenericPostRequestQuery } from 'src/store/api/app'
 import { useListDidsQuery } from 'src/store/api/ratelDids'
@@ -15,133 +21,100 @@ import { useListVariablesQuery, useListVariableQuery } from 'src/store/api/ratel
 import { useSelector } from 'react-redux'
 
 const Error = ({ name }) => (
-    <Field
-      name={name}
-      subscription={{ touched: true, error: true }}
-      render={({ meta: { touched, error } }) =>
-        touched && error ? (
-          <CCallout color="danger">
-            <FontAwesomeIcon icon={faExclamationTriangle} color="danger" />
-            {error}
-          </CCallout>
-        ) : null
-      }
-    />
-  )
+  <Field
+    name={name}
+    subscription={{ touched: true, error: true }}
+    render={({ meta: { touched, error } }) =>
+      touched && error ? (
+        <CCallout color="danger">
+          <FontAwesomeIcon icon={faExclamationTriangle} color="danger" />
+          {error}
+        </CCallout>
+      ) : null
+    }
+  />
+)
 
-  Error.propTypes = {
-    name: PropTypes.string.isRequired,
-  }
+Error.propTypes = {
+  name: PropTypes.string.isRequired,
+}
 
 export const AddRatelVariable = ({ children }) => {
-    const tenantDomain = useSelector((state) => state.app.currentTenant.customerId)
-    const [genericPostRequest, postResults ] = useLazyGenericPostRequestQuery()
+  const tenantDomain = useSelector((state) => state.app.currentTenant.customerId)
+  const [genericPostRequest, postResults] = useLazyGenericPostRequestQuery()
 
-    const onSubmit = async (values) => {
-      
-      let result = await genericPostRequest({
-          path: `/api/LtScheduleScript?TenantFilter=${tenantDomain}&Parameters=Key=astFamily|Value=${values.family},Key=astKey|Value=${values.key},Key=astValue|value=${values.value}&RatelScript=true&ScriptId=7355`
+  const onSubmit = async (values) => {
+    let result = await genericPostRequest({
+      path: `/api/LtScheduleScript?TenantFilter=${tenantDomain}&Parameters=Key=astFamily|Value=${values.family},Key=astKey|Value=${values.key},Key=astValue|value=${values.value}&RatelScript=true&ScriptId=7355`,
+    }).unwrap()
+    if (result.Data.Results.includes('Message=Success')) {
+      let syncRes = await genericPostRequest({
+        path: `/api/LtScheduleScript?TenantFilter=${tenantDomain}&RatelScript=true&ScriptId=7368`,
       }).unwrap()
-      if (result.Data.Results.includes("Message=Success")) {
-          let syncRes = await genericPostRequest({
-              path: `/api/LtScheduleScript?TenantFilter=${tenantDomain}&RatelScript=true&ScriptId=7368`
-          }).unwrap();
-          console.log('syncRes', syncRes)
-      }
+      console.log('syncRes', syncRes)
+    }
   }
 
   return (
-    <CippWizard
-    onSubmit={onSubmit}
-    wizardTitle="Add Ratel Variable"
-    >
-        <CippWizard.Page title="Choose A Tenant">
+    <CippWizard onSubmit={onSubmit} wizardTitle="Add Ratel Variable">
+      <CippWizard.Page title="Choose A Tenant">
         <center>
-        <h3 className="text-primary">Step 1</h3>
-        <h5 className="card-title mb-4">Choose a tenant</h5>
+          <h3 className="text-primary">Step 1</h3>
+          <h5 className="card-title mb-4">Choose a tenant</h5>
         </center>
         <hr className="my-4" />
         <Field name="selectedTenants">{(props) => <TenantSelector />}</Field>
         <Error name="selectedTenants" />
         <hr className="my-4" />
-        </CippWizard.Page> 
-        <CippWizard.Page title="Input New Variable Fields" description="">
+      </CippWizard.Page>
+      <CippWizard.Page title="Input New Variable Fields" description="">
         <center>
           <h3 className="text-primary">Step 2</h3>
           <h5>Input Family</h5>
         </center>
         <CRow>
-            <CCol lg={6} xs={12}>
-              <RFFCFormInput
-                label="Family"
-                name="family"
-                placeholder="Input Family"
-                />
-                </CCol>
-                <CCol lg={6} xs={12}>
-                <RFFCFormInput
-                label="Key"
-                name="key"
-                placeholder="Input Key"
-                />
-                </CCol>
-                <CCol lg={6} xs={12}>
-                <RFFCFormInput
-                label="Value"
-                name="value"
-                placeholder="Input Value"
-                />
-                </CCol>
+          <CCol lg={6} xs={12}>
+            <RFFCFormInput label="Family" name="family" placeholder="Input Family" />
+          </CCol>
+          <CCol lg={6} xs={12}>
+            <RFFCFormInput label="Key" name="key" placeholder="Input Key" />
+          </CCol>
+          <CCol lg={6} xs={12}>
+            <RFFCFormInput label="Value" name="value" placeholder="Input Value" />
+          </CCol>
         </CRow>
-        </CippWizard.Page>
-        <CippWizard.Page title="Confirm New Variable Values">
-        {/* <FormSpy subscription={{ values: true, labels: true }}>
-            {
-                
-                (props) => {
-                    const { values } = props
-                    const valuesArray = Object.keys(values).map((key) => ({
-                        key: key,
-                        values: values[key],
-                    }));
-                   let familyKey
-                   familyKey = valuesArray.find((item) => item.key === 'family')
-                   let keyKey
-                     keyKey = valuesArray.find((item) => item.key === 'key')
-                   let valueKey
-                       valueKey = valuesArray.find((item) => item.key === 'value')
-                    return (
-                        <CListGroup className="my-4">
-                            {
-                                familyKey && <CListGroupItem>
-                                    {`Family: ${familyKey.values}`}
-                                </CListGroupItem>
-                            }
-                            {
-                                keyKey && <CListGroupItem>
-                                    {`Key: ${keyKey.values}`}
-                                </CListGroupItem>
-                            }
-                            {
-                                valueKey && <CListGroupItem>
-                                    {`Value: ${valueKey.values}`}
-                                </CListGroupItem>
-                            }
-
-                        </CListGroup>
-                    )
-                }
-            }
-        </FormSpy> */}
+      </CippWizard.Page>
+      <CippWizard.Page title="Confirm New Variable Values">
+        <FormSpy subscription={{ values: true, labels: true }}>
+          {(props) => {
+            const { values } = props
+            const valuesArray = Object.keys(values).map((key) => ({
+              key: key,
+              values: values[key],
+            }))
+            console.log('valuesArray', valuesArray)
+            let familyKey
+            familyKey = valuesArray.find((item) => item.key === 'family')
+            let keyKey
+            keyKey = valuesArray.find((item) => item.key === 'key')
+            let valueKey
+            valueKey = valuesArray.find((item) => item.key === 'value')
+            return (
+              <CListGroup className="my-4">
+                {familyKey && <CListGroupItem>{`Family: ${familyKey.values}`}</CListGroupItem>}
+                {keyKey && <CListGroupItem>{`Key: ${keyKey.values}`}</CListGroupItem>}
+                {valueKey && <CListGroupItem>{`Value: ${valueKey.values}`}</CListGroupItem>}
+              </CListGroup>
+            )
+          }}
+        </FormSpy>
         {postResults.isSuccess && (
-              <CCallout color="success">
-                {/* <FontAwesomeIcon icon={faExclamationTriangle} color="success" /> */}
-                Variable Successfully Added
-              </CCallout>
-            )}
-        </CippWizard.Page>
-
-            
+          <CCallout color="success">
+            {/* <FontAwesomeIcon icon={faExclamationTriangle} color="success" /> */}
+            Variable Successfully Added
+          </CCallout>
+        )}
+      </CippWizard.Page>
     </CippWizard>
   )
 }
